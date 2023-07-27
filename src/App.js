dd9e04701491e572be6ec594c56376a4712a1348
Sean Haboon <116582145+shaboon@users.noreply.github.com>
2023-07-27 03:32:58 -0400

    Responsive Styling implemented
    Responsive styling tackled, centering issues present

--- src/App.js
+++ src/App.js
@@ -20,28 +20,30 @@ export default function App() {
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <div className="w-screen h-screen mt-0 bg-gray-800">
-      <div className="fixed mt-0">
+    <div className="w-screen h-screen mt-0 bg-gradient-to-b from-gray-600 to-gray-800">
+      <div className="fixed mt-0 filter drop-shadow-xl">
         <NavTabs
           currentPage={currentPage}
           handlePageChange={handlePageChange}
         />
       </div>
-      <div className="grid-columns-2 flex flex-1">
-        <div className="m-5 w-1/5 mt-20 flex flex-wrap self-center">
+      <div className="flex flex-col lg:flex-row">
+        {" "}
+        {/* Use flex classes for responsive layout */}
+        <div className="m-5 w-full lg:w-1/5 mt-20 lg:order-1">
           <img
-            className="selfie w-auto h-1/5 rounded-3x1"
+            className="selfie w-1/2 lg:w-auto h-1/5 lg:h-auto rounded-lg"
             src={selfie}
-            alt="picture of Sean Haboon"
+            alt="picture of (Me) Sean Haboon"
           />
-          <ul className="flex flex-wrap grid-column-1">
-            <li className="mr-4">
+          <ul className="flex flex-wrap grid-column-1 m-2 align-center bg-gradient-to-b from-gray-200 to-gray-400">
+            <li className="m-2 mr-4">
               <h1 className="text-red-400">Sean Haboon</h1>
             </li>
-            <li className="mr-4">shaboon.dev@gmail.com</li>
+            <li className="m-2 mr-4">shaboon.dev@gmail.com</li>
           </ul>
         </div>
-        <div className="w-4/5 m-5 mt-24 bg-gradient-to-b from-gray-100 to-gray-400 filter drop-shadow-lg">
+        <div className="w-full lg:w-4/5 m-5 mt-24 bg-gradient-to-b from-gray-100 to-gray-400 filter drop-shadow-lg lg:order-2">
           <Router>
             <Routes>
               <Route path="/" element={<About />} />
